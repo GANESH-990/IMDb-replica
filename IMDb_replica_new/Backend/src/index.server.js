5fbf941c0e6aac8d8374a5319dc6e3811f0b9607
Apoorva <40380945+Apoorvashirikanth@users.noreply.github.com>
2024-01-05 09:01:40 +0530

    deployment

--- IMDb_replica_new/Backend/src/index.server.js
+++ IMDb_replica_new/Backend/src/index.server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const env = require('dotenv');
 const cors = require('cors'); 
+const path = require("path");
 env.config();
 
 const app = express();
@@ -25,6 +26,19 @@ mongoose.connect(
 
 app.use(express.json());
 app.use(cors());
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    );
+  } else {
+    app.get("/", (req, res) => {
+      res.send("API is running");
+    });
+  }
 
 app.use('/api', moviesRoute);
 app.use('/api', actorsRoute);
