1cffcb1e65c20a935b3e72939f0785bb043c0b1e
GANESH-990 <gdhage86@gmail.com>
2024-01-06 11:53:01 +0530

    Modified config

--- IMDb_replica_new/Frontend/vite.config.js
+++ IMDb_replica_new/Frontend/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:4500}
+  server:{
+    port:4500,
+  host:'0.0.0.0',
+}
 })
